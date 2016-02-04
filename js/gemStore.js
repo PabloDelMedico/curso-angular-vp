364268673f31f1469395e823353c66ba20cfbe41
Pablo Del Médico <pablo.delmedico@globant.com>
2016-02-04 17:32:15 -0300

    Adding bootstrap files, arranging files structure

--- js/gemStore.js
+++ js/gemStore.js
@@ -1,7 +1,15 @@
 var app = angular.module('gemStore', []);
 
-var gem = { name: 'Azurite', price: 2.95 };
+var gem = {
+    name: 'Azurite',
+    price: 2.95,
+    canPurchase: false,
+    soldOut: false
+};
 
-app.controller('StoreController', function() {
+app.controller('StoreController', function($log) {
     this.gem = gem;
+    this.purchase = function() {
+        $log.warn('Purchase function not implemented yet');
+    };
 });
